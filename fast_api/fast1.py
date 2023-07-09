5ff834144d096c921c1c6694881db6178fba1713
unknown <romualdjja@gmail.com>
2023-07-09 10:08:29 +0200

    encore un on n en a jamais terminer

--- fast_api/fast1.py
+++ fast_api/fast1.py
@@ -17,44 +17,15 @@ async def datapi():
     db=client["finale"]
     collection6=db['valuebet']
     data=list(collection6.find({},{"_id":0}))
-    newdata=list(filter(lambda x:x["valeur"]<1.89,data))
+    newdata=list(filter(lambda x:x["valeur"]<2.21,data))
     return newdata
    
-
-
-@app.get("/indexsurebet")
-async def read_root2():
-    return FileResponse("/home/romualdjja/projet1/projet1/fast_api/templates/indexsurebet.html")
-
-
-
-
-
-@app.get("/")
-async def read_root():
-    return FileResponse("/home/romualdjja/projet1/projet1/fast_api/templates/index.html")
 
 
 # @app.get("/mini")
 # async def read_root1():
 #     return FileResponse("/home/romualdjja/projet1/projet1/fast_api/templates/indexmini.html")
 
-
-@app.get("/item/{item_id}")
-async def valueApi(item_id:str):
-
-
-
-    db_match_odd=client["finale"]
-    collection1=db_match_odd["Match Odds"]
-    #data=list(collection1.find({},{"_id":0}))
-    collection2=db_match_odd[item_id]
-    collection3=db_match_odd[item_id]
-    collection4=db_match_odd[item_id]
-    result1=list(collection4.find({},{"_id":0}))
-    #result2=list(collection5.find({},{"_id":0}))
-    #data=filter(lambda x :x["id"] not in [ i["id"] for i in result2],result1)
-    return list(collection4.find({},{"_id":0}))
 
 @app.post("/item/retour")
 async def create_item(request: Request):
@@ -103,4 +74,4 @@ async def create_item1(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app,host="0.0.0.0",port=8000)
+    uvicorn.run(app,host="0.0.0.0",port=8001)
