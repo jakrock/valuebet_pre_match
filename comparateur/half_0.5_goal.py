ab0ffd28b29056003c88598f64f1fcdaa860024e
unknown <romualdjja@gmail.com>
2023-08-02 16:29:47 +0200

    le commit de changement encore un commit

--- comparateur/half_0.5_goal.py
+++ comparateur/half_0.5_goal.py
@@ -461,8 +461,8 @@ batch_size = 20
 max_concurrent_tasks = 20
 
 # Appel de la fonction asynchrone pour traiter l'ensemble de données
-#loop = asyncio.get_event_loop()
-#loop.run_until_complete(process_data_set(resultat, batch_size, max_concurrent_tasks))
+loop = asyncio.get_event_loop()
+loop.run_until_complete(process_data_set(resultat, batch_size, max_concurrent_tasks))
 
 
 client.close()
